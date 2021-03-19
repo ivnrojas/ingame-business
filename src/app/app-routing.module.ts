713d6fe1767a9728cf4b791a932b0c0402ff1428
Yamil Foglia <yamil.foglia@outlook.com>
2021-03-19 18:46:31 -0300

    add path in routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RouletteComponent } from './fetures/roulette/roulette.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'roulette', component: RouletteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
