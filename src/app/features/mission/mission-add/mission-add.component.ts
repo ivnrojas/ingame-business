2a06cf4ef3190629ad54e1873d6ebd9a7d5adf4b
EmaSG <EmaSG@outlook.es>
2021-03-20 20:06:20 -0300

    added formControls

--- src/app/features/mission/mission-add/mission-add.component.ts
+++ src/app/features/mission/mission-add/mission-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { missionCategory, MissionLevel } from 'src/app/core/entities';
 
 @Component({
@@ -8,9 +9,32 @@ import { missionCategory, MissionLevel } from 'src/app/core/entities';
 })
 export class MissionAddComponent implements OnInit {
 
+     public missionForm: FormGroup;
+
+     public titleControl = new FormControl(null, Validators.required);
+     public levelControl = new FormControl(MissionLevel.one,Validators.required);
+     public descriptionControl = new FormControl(null,Validators.required);
+     public maxDayControl = new FormControl(null,Validators.required);
+     public categoryControl = new FormControl(missionCategory.legal,Validators.required);
+     public companyProfitControl = new FormControl(null,Validators.required);
+     public userExperienceControl = new FormControl(null,Validators.required);
+     
     constructor() { }
 
     ngOnInit(): void {
+        this.initializeTaskForm();
+    }
+
+    private initializeTaskForm(): void {
+        this.missionForm = new FormGroup({
+            title: this.titleControl,
+            level:this.levelControl,
+            description: this.descriptionControl,
+            maxDay: this.maxDayControl,
+            category: this.categoryControl,
+            companyProfit: this.companyProfitControl,
+            userExperience:this.userExperienceControl
+        });
     }
 
     public getLevelMissionKeys(): string[] {
